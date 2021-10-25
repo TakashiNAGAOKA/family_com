db1e301e682c32ff1c0c903a192adb100aaa8691
takashi.nagaoka <takashi.nagaoka@gmail.com>
2021-10-25 22:31:09 +0000

    note/show action create comment add

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,29 @@
 class CommentsController < ApplicationController
-
     def new
         
     end
 
-    def new
-        
+  def create
+#   @comment = Comment.find(current_user.family_id)
+#    binding.pry
+
+   @comment = Comment.new
+   @comment.user_id = current_user.id
+   @comment.note_id = params[:id]
+   @comment.comment = params[:comment]
+   
+#    binding.pry
+
+    if (@comment.save)
+      flash[:success] = 'コメントを登録しました。'
+      redirect_to user_note_path
+      
+    else
+      flash.now[:danger] = 'コメントの登録に失敗しました。'
+      redirect_to user_note_path
     end
+#   redirect_to user_note_path
+  end
 
     def new
         
--- config/routes.1.rb
+++ config/routes.1.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:index, :show, :create] do
-  resources :notes, only: [:new,:create,:show,:edit]
+  resources :notes, only: [:new,:create,:show,:edit] do
+    resources :comments, only: [:new,:create,:edit]
+    end
   resources :subjects, only: [:show]
   end
-
-  post '/users/:user_id/notes/:id', to: 'comments#create', as: 'comment'
 
   resources :families, only: [:new,:create,:show,:edit,:update]
 # patch 'add_family_user' ,to: 'families#????'  
