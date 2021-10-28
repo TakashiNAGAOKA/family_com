ea131bdbcf965721c845863abeb4c5a0988ca423
takashi.nagaoka <takashi.nagaoka@gmail.com>
2021-10-28 03:03:10 +0000

    for some issue modify2.2

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :comment, presence: true, length: { maximum: 1000 }
+  validates :comment, presence: true, length: { maximum: 255 }
 
   belongs_to :note
   belongs_to :user
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
-  validates :subject_id, presence: true, length: { maximum: 10 }
-  validates :status, presence: true, length: { maximum: 50 }
-  validates :note, presence: true, length: { maximum: 1000 }
+  validates :subject_id, presence: true, length: { maximum: 20 }
+  validates :status, presence: true, length: { maximum: 255 }
+  validates :note, presence: true, length: { maximum: 255 }
 
   belongs_to :subject
   belongs_to :user
