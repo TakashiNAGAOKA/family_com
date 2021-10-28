8b0597a33fb291834484f0fd20d35dc85346c86b
takashi.nagaoka <takashi.nagaoka@gmail.com>
2021-10-28 10:42:18 +0000

    before error implement

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,10 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :users, only: [:index, :show, :create] do
-  resources :notes, only: [:new,:create,:show,:edit]
-  resources :subjects, only: [:show]
+  resources :notes, only: [:new,:create,:show]
   end
 
-  post '/users/:user_id/notes/:id', to: 'comments#create', as: 'comment'
+  post '/users/:user_id/notes/:id', to: 'comments#create', as: 'comments'
 
   resources :families, only: [:new,:create,:show]
 # patch 'add_family_user' ,to: 'families#????'  
